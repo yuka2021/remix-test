f965742e9956315b23023273529d174d57824176
Yuka Ishiwata Dev2 <yuka.ishiwata.dev@gmail.com>
2022-06-16 12:09:23 -0700

    Add posts page

--- app/root.jsx
+++ app/root.jsx
@@ -18,7 +18,6 @@ export default function App() {
       <Layout>
         <Outlet />
       </Layout>
-      <Outlet />
     </Document>
   );
 }
---
+++ app/routes/posts.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Posts = () => {
+  return (
+    <div>
+      <h1>This is the posts route</h1>
+    </div>
+  );
+};
+
+export default Posts;
