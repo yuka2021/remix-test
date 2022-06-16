775299982959585bb0f39a5d8a98f9e9f1250ad1
Yuka Ishiwata Dev2 <yuka.ishiwata.dev@gmail.com>
2022-06-16 12:02:13 -0700

    Add layout and meta

--- app/root.jsx
+++ app/root.jsx
@@ -1,8 +1,23 @@
-import { Outlet, LiveReload } from "remix";
+import { Outlet, LiveReload, Link, Links, Meta } from "remix";
+import globalStylesUrl from "~/styles/global.css";
+
+export const links = () => [{ rel: "stylesheet", href: globalStylesUrl }];
+export const meta = () => {
+  const description = "A cool blog built with Remix";
+  const keywords = "Remix, Blog";
+
+  return {
+    description,
+    keywords,
+  };
+};
 
 export default function App() {
   return (
     <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
       <Outlet />
     </Document>
   );
@@ -12,6 +27,9 @@ function Document({ children, title }) {
   return (
     <html lang="en">
       <head>
+        <Meta />
+        {/* <link rel="stylesheet" href={globalStylesUrl} /> */}
+        <Links />
         <title>{title ? title : "My Remix Blog"}</title>
         <body>
           {children}
@@ -19,5 +37,25 @@ function Document({ children, title }) {
         </body>
       </head>
     </html>
+  );
+}
+
+function Layout({ children }) {
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="logo">
+          My Remix
+        </Link>
+      </nav>
+
+      <ul className="nav">
+        <li>
+          <Link to="posts">Posts</Link>
+        </li>
+      </ul>
+
+      <div className="container">{children}</div>
+    </>
   );
 }
